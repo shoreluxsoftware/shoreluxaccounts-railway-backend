9a9203d65a885491a0a05afd59de35c37190cec8
shoreluxsoftware <shoreluxsoftware@gmail.com>
2026-01-05 15:26:46 +0530

    changed allowed hosts from env

--- shorelux/settings.py
+++ shorelux/settings.py
@@ -30,10 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-for-dev-only")
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
 
 
 # Application definition
