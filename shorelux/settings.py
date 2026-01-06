3423e128dbd83ffdfeeb8a2b6554ed670d4127e9
shoreluxsoftware <shoreluxsoftware@gmail.com>
2026-01-06 09:07:08 +0530

    did i did any changes

--- shorelux/settings.py
+++ shorelux/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join("/data", "staticfiles")
+STATIC_ROOT = os.path.join("/app/media", "staticfiles")
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -179,7 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join("/data", "media")
+MEDIA_ROOT = os.path.join("/app/media", "media")
 # MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
